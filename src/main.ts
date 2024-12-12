ff177e5a9a8608b1b28ffaa86e96bb41e7f02c88
Radekor500 <radek00500@gmail.com>
2024-12-12 21:58:55 +0100

    move carousel

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
 import './style.scss'
 
+enum Direction {
+  Right,
+  Left
+}
 
+const btnLeft = document.getElementById('btn-left') as HTMLElement;
+const btnRight = document.getElementById('btn-right') as HTMLElement;
+
+const carousel = document.querySelector('.carousel .carousel--list') as HTMLElement;
+
+function move(direction: Direction) {
+  console.log('moveRight');
+  const itemWidth = carousel.clientWidth || 0;
+  const gap = 16; // 1rem = 16px
+
+  carousel.scrollBy({
+    left: direction === Direction.Right ? itemWidth + gap : -(itemWidth + gap),
+    behavior: 'smooth'
+  });
+}
+
+btnRight.addEventListener('click', () => move(Direction.Right));
+btnLeft.addEventListener('click', () => move(Direction.Left));
